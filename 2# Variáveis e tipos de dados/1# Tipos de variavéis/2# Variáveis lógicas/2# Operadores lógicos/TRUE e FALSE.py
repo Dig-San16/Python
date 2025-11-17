8a71f97bdaa5d562c8be86b649a70457c3d8915d
DigSan <digsantos.hoode@gmail.com>
2025-11-16 23:23:49 -0300

    Update TRUE e FALSE.py

--- 2# Variáveis e tipos de dados/1# Tipos de variavéis/2# Variáveis lógicas/2# Operadores lógicos/TRUE e FALSE.py
+++ 2# Variáveis e tipos de dados/1# Tipos de variavéis/2# Variáveis lógicas/2# Operadores lógicos/TRUE e FALSE.py
@@ -26,6 +26,9 @@ print(salário2 > 1000 and idade2 > 18) #2
 
 #Na primeira situação, o resultado será FALSO, pois:
 #   "salario" < 1000 e "idade" > 18
+#         FALSE    AND     TRUE
+
 
 #Na segunda situação, o resultado será VERDADEIRO, pois:
 #   "salario2" > 1000 e "idade2" > 18
+#          TRUE     AND      TRUE
